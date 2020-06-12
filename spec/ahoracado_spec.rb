7fad3d8d935db1c73e942234237b81442b35a527
santiagoHV <santiherreravelas@gmail.com>
2020-06-12 13:56:09 +0000

    añadida la prueba unitaria '...debe devolver rayitas'

--- spec/ahoracado_spec.rb
+++ spec/ahoracado_spec.rb
@@ -9,5 +9,13 @@ describe VerificarCaracter do
         #assert
         expect(result).to eq "Correcto!"
     end
-    
+    it "si la palabra secreta es pescado debe devolver '_ _ _ _ _ _ _'" do
+       #Arrange
+       verificar = VerificarCaracter.new
+       
+       resultado = verificar.guardarSecreto("pescado")
+
+       expect(resultado).to eq "_ _ _ _ _ _ _"
+
+    end
 end
